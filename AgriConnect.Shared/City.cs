18250285da4748bf96ea6fa008cd2510bd12a3a4
MANUEL ANTONIO GUZMAN SANCHEZ <191478@iberopuebla.mx>
2025-02-10 12:00:02 -0600

    Primero crear repositorio Clonacion del repositorio Crear la solucion en blanco Crea proycto Bibilioteca de clases Crear proyecto web crear proyecto mobile Crear modelo en el proyecto compartido (entities) instalar paquetes (microsoft.entity.framework. core.tools) crear la clase datacontext en el proyecto web crear cadena de conecion en appsetings crear el servicio datacontext en la clase program.cs Crear migraciones Add-Migration initial Crear base de datos Update-Database (seleccionar proyecto web)

---
+++ AgriConnect.Shared/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriConnect.Shared
+{
+    public class City
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name="Ciudad")]
+        [MaxLength(70, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres")]
+
+        //sirve para admitir valores nulos -> ?
+        public string? Name { get; set; }
+    }
+}
--- AgriConnect.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AgriConnect.Shared
-{
-    public class Class1
-    {
-
-    }
-}
